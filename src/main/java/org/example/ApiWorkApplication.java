24273e9c3e4d9d14435adc1c94032c69a550b1c7
secondcitysam <samyak.kedari4444@gmail.com>
2026-04-20 16:27:19 +0530

    setup

---
+++ src/main/java/org/example/ApiWorkApplication.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiWorkApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ApiWorkApplication.class,args);
+
+
+    }
+
+
+
+
+}
